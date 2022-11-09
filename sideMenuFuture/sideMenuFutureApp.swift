3cdf082571a9df12d886a7a8402f48133e4589ee
Sinz83 <petitcomiteparis@gmail.com>
2022-11-09 09:48:36 +0100

    Initial Commit

---
+++ sideMenuFuture/sideMenuFutureApp.swift
@@ -0,0 +1,17 @@
+//
+//  sideMenuFutureApp.swift
+//  sideMenuFuture
+//
+//  Created by Sabrina Inzillo on 09.11.22.
+//
+
+import SwiftUI
+
+@main
+struct sideMenuFutureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
